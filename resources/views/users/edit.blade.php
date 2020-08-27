70f225b9d3edd442f561738eb920e4b53b960db0
WiloCh <69049094+WiloCh@users.noreply.github.com>
2020-08-27 00:29:33 -0500

    Cambios en el CRUD de Users

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -8,70 +8,67 @@
         </div>
         <div class="card-body">
 
-    @if($errors->any())
-    <ul>
-        @foreach($errors->all() as $error)
-        <li class="text-danger">{{ $error }}</li>
-        @endforeach
-    </ul>
+            @if($errors->any())
+            <ul>
+                @foreach($errors->all() as $error)
+                <li class="text-danger">{{ $error }}</li>
+                @endforeach
+            </ul>
 
-    @endif
+            @endif
 
-    <form action="{{route('users.update',['user'=>$user->id])}}" method="POST" novalidate>
-        @csrf
-        @method('PUT')
-        
+            <form action="{{route('users.update',['user'=>$user->id])}}" method="POST" novalidate>
+                @csrf
+                @method('PUT')
 
-                                        <div class="form-group">
-            <label for="name">Name</label>
-                    <input class="form-control String"  type="text"  name="name" id="name" value="{{old('name',$user->name)}}"
-                                    required="required"
-                        >
+
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input class="form-control String" type="text" name="name" id="name"
+                        value="{{old('name',$user->name)}}" required="required">
                     @if($errors->has('name'))
-            <p class="text-danger">{{$errors->first('name')}}</p>
-            @endif
-        </div>
-                                <div class="form-group">
-            <label for="email">Email</label>
-                    <input class="form-control String"  type="text"  name="email" id="email" value="{{old('email',$user->email)}}"
-                                    required="required"
-                        >
+                    <p class="text-danger">{{$errors->first('name')}}</p>
+                    @endif
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input class="form-control String" type="text" name="email" id="email"
+                        value="{{old('email',$user->email)}}" required="required">
                     @if($errors->has('email'))
-            <p class="text-danger">{{$errors->first('email')}}</p>
-            @endif
-        </div>
-                                <div class="form-group">
-            <label for="email_verified_at">Email Verified At</label>
-                    <input class="form-control DateTime"  type="text"  name="email_verified_at" id="email_verified_at" value="{{old('email_verified_at',$user->email_verified_at)}}"
-                                    >
+                    <p class="text-danger">{{$errors->first('email')}}</p>
+                    @endif
+                </div>
+                <div class="form-group">
+                    <label for="email_verified_at">Email Verified At</label>
+                    <input class="form-control DateTime" type="text" name="email_verified_at" id="email_verified_at"
+                        value="{{old('email_verified_at',$user->email_verified_at)}}">
                     @if($errors->has('email_verified_at'))
-            <p class="text-danger">{{$errors->first('email_verified_at')}}</p>
-            @endif
-        </div>
-                                <div class="form-group">
-            <label for="password">Password</label>
-                    <input class="form-control String"  type="text"  name="password" id="password" value="{{old('password',$user->password)}}"
-                                    required="required"
-                        >
+                    <p class="text-danger">{{$errors->first('email_verified_at')}}</p>
+                    @endif
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input class="form-control String" type="text" name="password" id="password"
+                        value="{{old('password',$user->password)}}" required="required">
                     @if($errors->has('password'))
-            <p class="text-danger">{{$errors->first('password')}}</p>
-            @endif
-        </div>
-                                <div class="form-group">
-            <label for="remember_token">Remember Token</label>
-                    <input class="form-control String"  type="text"  name="remember_token" id="remember_token" value="{{old('remember_token',$user->remember_token)}}"
-                                    >
+                    <p class="text-danger">{{$errors->first('password')}}</p>
+                    @endif
+                </div>
+                <div class="form-group">
+                    <label for="remember_token">Remember Token</label>
+                    <input class="form-control String" type="text" name="remember_token" id="remember_token"
+                        value="{{old('remember_token',$user->remember_token)}}">
                     @if($errors->has('remember_token'))
-            <p class="text-danger">{{$errors->first('remember_token')}}</p>
-            @endif
+                    <p class="text-danger">{{$errors->first('remember_token')}}</p>
+                    @endif
+                </div>
+                <div>
+                    <button class="btn btn-success" type="submit">Grabar</button>
+                    <a href="{{route('users.index')}}" class="btn btn-primary">Regresar</a>
+                </div>
+            </form>
         </div>
-                                                        <div>
-            <button class="btn btn-success" type="submit">Grabar</button>
-            <a href="{{route('users.index')}}" class="btn btn-primary">Regresar</a>
-        </div>
-    </form>
     </div>
-        </div>
 
 </div>
 @endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -9,30 +9,34 @@
                 <div class="col-8">
                     <h4>Ver User</h4>
                 </div>
-                <div class="col-4" style="text-align:right;padding-right:15px"> 
+                <div class="col-4" style="text-align:right;padding-right:15px">
                     <a href="{{route('users.index')}}" class="btn btn-primary">Regresar</a>
                 </div>
             </div>
         </div>
 
-    <div class="card-body">
-                                        <div class="form-group">
-            <label class="col-form-label" for="value">Name</label>
-            <input type="text" readonly class="form-control" id="staticEmail" value="{{$user->name}}">
+        <div class="card-body">
+            <div class="form-group">
+                <label class="col-form-label" for="value">Name</label>
+                <input type="text" readonly class="form-control" id="staticEmail" value="{{$user->name}}">
+            </div>
+            <div class="form-group">
+                <label class="col-form-label" for="value">Email</label>
+                <input type="text" readonly class="form-control" id="staticEmail" value="{{$user->email}}">
+            </div>
+            <div class="form-group">
+                <label for="value" class="card-title">Role</label>
+                <p class="card-text">
+                    .......
+                </p>
+            </div>
+            <div class="form-group">
+                <label for="value" class="card-title">Permissions</label>
+                <p class="card-text">
+                    .......
+                </p>
+            </div>
         </div>
-                                <div class="form-group">
-            <label class="col-form-label" for="value">Email</label>
-            <input type="text" readonly class="form-control" id="staticEmail" value="{{$user->email}}">
-        </div>
-                                                <div class="form-group">
-            <label class="col-form-label" for="value">Password</label>
-            <input type="text" readonly class="form-control" id="staticEmail" value="{{$user->password}}">
-        </div>
-                                <div class="form-group">
-            <label class="col-form-label" for="value">Remember Token</label>
-            <input type="text" readonly class="form-control" id="staticEmail" value="{{$user->remember_token}}">
-        </div>
-                                                    </div>
 
     </div>
 </div>
